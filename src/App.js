1ecf6f7c27e7cdb103dec78d07415840b0ebff0a
unknown <almusallam296@hotmail.com>
2023-05-28 08:21:07 -0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import img0 from "./assets/img/cookie.jpg";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Cookies and Beyond </h1>
+      <p> Where cookie maniacs gather </p>
+      <img src={img0} alt="cookie" />
     </div>
   );
 }
