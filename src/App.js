901e82d8571680f6196495fe788a3a06c39c7afb
unknown <almusallam296@hotmail.com>
2023-05-28 19:22:02 -0700

    Done task2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
 import img0 from "./assets/img/cookie.jpg";
+import products from "./products";
 function App() {
+  const productsList = products.map((product) => {
+    return (
+      <div className="DownPic">
+        <img src={product.image} height={"150px"} width={"200px"} />
+        <p> {product.name} </p>
+        <p> {product.price}</p>
+      </div>
+    );
+  });
+
   return (
     <div className="App">
       <h1> Cookies and Beyond </h1>
       <p> Where cookie maniacs gather </p>
       <img src={img0} alt="cookie" />
+      {productsList}
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,15 @@
+import cookie_1 from "./assets/img/chip.jpg";
+import cookie_2 from "./assets/img/cute.jpg";
+const products = [
+  {
+    name: "Chocolate Chip Cookies",
+    price: "10 KD",
+    image: cookie_1,
+  },
+  {
+    name: "Cute Cookies",
+    price: "9 KD",
+    image: cookie_2,
+  },
+];
+export default products;
